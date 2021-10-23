ff51e4f9ecfc21783247dd28fbed4b4ef6a04a95
João Pedro de Almeida <joaopedro.alm.c@gmail.com>
2021-10-22 23:05:06 -0300

    0.3
    consegui fazer o range para se mover

--- script/script.js
+++ script/script.js
@@ -1,26 +1,19 @@
-const container = document.querySelectorAll('.bar-value');
+const slider = document.getElementById('myRange');
+const valor = document.getElementById('pay-month');
+const pages = document.getElementById('pages');
 
-for (let i = 0; i < container.length; i++) {
-	// função que manuseia os valores do slider
-	const slider = container[i].querySelector('.range');
-	const thumb = container[i].querySelector('.slider-thumb');
-	const quadrado = container[i].querySelector('.quadrado');
-	const progress = container[i].querySelector('.progress');
-
-	function customSlider() {
-		// pega porcentagem
-		const maxVal = slider.getAttribute('max');
-		const val = (slider.value / maxVal) * 100 + '%';
-
-		// vai mostrar o valor no tooltip
-		quadrado.innerHTML = slider.value;
-		// defini o valor do quadrado e progress de forma correta
-		progress.style.width = val;
-		thumb.style.left = val;
-	}
-	customSlider();
-	// essa função vai funcionar quando o estiver em execução
-	slider.addEventListener('input', () => {
-		customSlider();
-	});
+// vai pegar os valores do input e imprimi-las na pagina
+function writeSlider() {
+	valor.innerHTML = parseFloat(slider.value).toFixed(2).replace('.', ',');
+	pages.innerHTML = `${parseInt(slider.value)}k`;
 }
+// vai alterar a cor de fundo do input range
+slider.addEventListener('mousemove', function () {
+	// aqui vai fazer um calculo de porcentagem, já que o mesmo é para isso
+	let porcent = Number((slider.value - slider.min) / (slider.max - slider.min) * 100);
+	slider.style.background = `linear-gradient(
+		90deg,
+		rgb(183, 1, 255, 0.801) ${porcent}%,
+		rgba(79, 0, 143, 0.801) ${porcent}%
+	)`
+});
