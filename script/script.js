2fae683110a3e22c331404136bfcf25fdc3776b5
João Pedro de Almeida <joaopedro.alm.c@gmail.com>
2021-10-25 22:04:38 -0300

    comecei a estilizar o checkbox

--- script/script.js
+++ script/script.js
@@ -10,10 +10,27 @@ function writeSlider() {
 // vai alterar a cor de fundo do input range
 slider.addEventListener('mousemove', function () {
 	// aqui vai fazer um calculo de porcentagem, já que o mesmo é para isso
-	let porcent = Number((slider.value - slider.min) / (slider.max - slider.min) * 100);
+	let porcent = Number(
+		((slider.value - slider.min) / (slider.max - slider.min)) * 100
+	);
 	slider.style.background = `linear-gradient(
 		90deg,
 		rgb(183, 1, 255, 0.801) ${porcent}%,
 		rgba(79, 0, 143, 0.801) ${porcent}%
-	)`
+	)`;
 });
+
+// ANIMAÇÃO DO INPUT CHECHBOX
+const box = document.getElementById('checkBox');
+const ball = document.getElementById('ball');
+
+box.addEventListener('click', function(){
+	if (this.checked === false) {
+		// console.log(this.checked)
+		ball.style.left =`4px`
+	} else {
+		ball.style.left =`52%`
+	}
+});
+
+
