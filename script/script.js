4a0760e4138fb0716d84e76a4144a47bbea722d7
João Pedro de Almeida <joaopedro.alm.c@gmail.com>
2021-10-26 19:33:25 -0300

    testantdo slider no moble

--- script/script.js
+++ script/script.js
@@ -8,7 +8,7 @@ function writeSlider() {
 	pages.innerHTML = `${parseInt(slider.value)}k`;
 }
 // vai alterar a cor de fundo do input range *** nao executa quando no telefone
-slider.addEventListener('mousemove', function () {
+slider.addEventListener('input', function () {
 	// aqui vai fazer um calculo de porcentagem, já que o mesmo é para isso
 	let porcent = Number(
 		((slider.value - slider.min) / (slider.max - slider.min)) * 100
@@ -17,22 +17,27 @@ slider.addEventListener('mousemove', function () {
 		90deg,
 		rgb(183, 1, 255, 0.801) ${porcent}%,
 		rgba(79, 0, 143, 0.801) ${porcent}%
-	)`;
+	)`;	
+	// if (this.value <= 0) {
+	// 	slider.style.opacity = '0.7';
+	// 	slider.style.transition = '0.5s'		
+	// }else if(this.value > 0){
+	// 	slider.style.transition = '0.5s'		
+	// 	slider.style.opacity = '1';
+	// }
 });
 
 // ANIMAÇÃO DO INPUT CHECHBOX
 const box = document.getElementById('checkBox');
 const ball = document.getElementById('ball');
 
-box.addEventListener('click', function(){
+box.addEventListener('click', function () {
 	if (this.checked === false) {
 		// console.log(this.checked)
-		ball.style.left =`4px`
-		ball.style.transition =`0.5s`
+		ball.style.left = `4px`;
+		ball.style.transition = `0.5s`;
 	} else {
-		ball.style.left =`52%`
-		ball.style.transition =`0.5s`
+		ball.style.left = `52%`;
+		ball.style.transition = `0.5s`;
 	}
 });
-
-
